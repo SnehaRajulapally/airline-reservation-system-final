f194af1bf0608f6865c80d169337b692674266d1
sneha <sneha@DESKTOP-HVQP8I4>
2020-04-19 16:55:08 -0400

    Login view and controller commit

--- AirlineReservationSystem/src/application/Main.java
+++ AirlineReservationSystem/src/application/Main.java
@@ -7,18 +7,22 @@ import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 
 public class Main extends Application {
+	public static Stage stage; 
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			// Load the UI elements
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/LoginView.fxml"));
-			AnchorPane root =  (AnchorPane) loader.load();
-			Scene scene = new Scene(root,600,400);
-			primaryStage.setTitle("Illinois Tech Airways");
+			stage = primaryStage;
+			AnchorPane root = (AnchorPane) 
+	        FXMLLoader.load(getClass().getResource("/views/LoginView.fxml"));
+			Scene scene = new Scene(root);
+	        scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setTitle("Illinois Tech Airways Login Page");
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
+			System.out.println("Error in inflating view: " + e);
 			e.printStackTrace();
+			
 		}
 	}
 	
---
+++ AirlineReservationSystem/src/dao/DBConnect.java
@@ -0,0 +1,18 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnect {
+	protected Connection connection;
+	public Connection getConnection() { return connection; }
+
+	static final String DB_URL = "jdbc:mysql://www.papademas.net:3307/510labs?autoReconnect=true&useSSL=false";
+	static final String USER = "db510", PASS = "510"; // login credentials
+	
+	public Connection connect() throws SQLException 
+	{
+	 return DriverManager.getConnection(DB_URL, USER, PASS);   // error to display if the connection fails
+	}
+}
